7319b3a841f222b7f325b6858e2dbc8ec3f9fb13
Nguyen Phuc Bao Nguyen <nguyennpb.1808@gmail.com>
2024-11-09 00:19:09 +0700

    Menu
    Tạo menu , thêm background và tạo audio manager

---
+++ Assets/scriptMenu/MEnu/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject settingsCanvas;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    public void Settings()
+    {
+        settingsCanvas.SetActive(true); 
+    }
+
+    public void CloseSettings()
+    {
+        settingsCanvas.SetActive(false); 
+    }
+}
+
